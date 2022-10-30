57d4c10fe70522f9e0b0d015497352ef53d92f63
lbayarkhuu <l.bayarkhuu@gmail.com>
2022-10-30 16:43:06 -0400

    Assignment-3-Lesson-4-WalmartLoginPage

---
+++ Lesson4WalmartLogin/app/src/main/java/com/example/lesson_4_walmartlogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lesson_4_walmartlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
